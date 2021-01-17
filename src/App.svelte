3bb9a91fbf9a901b669084180f8e046868cc9c2d
Francesco Pezzella <franzpezzella@gmail.com>
2021-01-17 12:59:40 +0100

    bootstrapping

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,24 @@
 <script>
-  export let name;
+  import Welcome from "./screens/Welcome.svelte";
+  let state = "welcome";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  {#if state === "welcome"}
+    <Welcome />
+  {:else if state === "playing"}
+    <div>Y</div>
+  {/if}
 </main>
 
 <style>
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
   }
 </style>
