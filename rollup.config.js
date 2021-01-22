338d1b48b800b1ae5cdb83406ea0f799f5318046
Francesco Pezzella <franzpezzella@gmail.com>
2021-01-22 15:19:07 +0100

    feat: service worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import svelte from 'rollup-plugin-svelte'
 import css from 'rollup-plugin-css-only'
+import replace from '@rollup/plugin-replace'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
@@ -7,54 +8,68 @@ import { terser } from 'rollup-plugin-terser'
 
 const production = !process.env.ROLLUP_WATCH
 
-export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js'
-  },
-  plugins: [
-    svelte({
-      emitCss: true,
-      compilerOptions: {
+export default [
+  {
+    input: 'src/main.js',
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      name: 'app',
+      file: 'public/build/bundle.js'
+    },
+    plugins: [
+      svelte({
+        emitCss: true,
+        compilerOptions: {
         // enable run-time checks when not in production
-        dev: !production
-      }
-    }),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
-    css({
-      output: 'bundle.css'
-    }),
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
-    commonjs(),
+          dev: !production
+        }
+      }),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css({
+        output: 'bundle.css'
+      }),
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
 
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
+      // In dev mode, call `npm run start` once
+      // the bundle has been generated
+      !production && serve(),
 
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload('public'),
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload('public'),
 
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
-  ],
-  watch: {
-    clearScreen: false
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  },
+  {
+    input: 'src/service-worker.js',
+    output: {
+      file: 'public/service-worker.js',
+      format: 'esm'
+    },
+    plugins: [
+      replace({
+        TIMESTAMP: Date.now()
+      })
+    ]
   }
-}
+]
 
 function serve () {
   let started = false
